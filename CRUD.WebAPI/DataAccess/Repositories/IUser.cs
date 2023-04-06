4b31a23eeb99f5098dc03d6ebdcedda3bf072fa9
Rafael Marques <rafaelmarques.rm86@gmail.com>
2023-04-06 12:18:14 -0300

    Criação do data com seus atributos e interface do data

---
+++ CRUD.WebAPI/DataAccess/Repositories/IUser.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD.WebAPI.DataAccess.Repositories
+{
+    public interface IUser
+    {
+        Task<List<Data.User>> GetAll();
+        Task<Data.User> GetUserById(string id);
+        string AddUser(Data.User user);
+        Task UpdateUser(Data.User user);
+        Task DeleteUser(Data.User user);
+    }
+}
